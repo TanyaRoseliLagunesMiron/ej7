044af58bbd2a9e645ca3bf1ff18e63a29043eaff
TanyaRoseliLagunesMiron <almn.tanyalagunes@cbtis66.edu.mx>
2022-10-12 20:40:05 -0500

    version7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ej7';
+  mensaje='';
+
+  actualizar(t: number) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }
 }
